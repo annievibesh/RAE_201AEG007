a0d60bdc76782659d00e7660f3ba4c785a14d546
annievibesh <63414241+annievibesh@users.noreply.github.com>
2020-05-18 00:46:46 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, request
-from flask_restful import Resource, Api
-from flask_jwt import JWT ,jwt_required
+from flask import Flask
+from flask_restful import  Api
+from flask_jwt import JWT
 from security import authenticate, identity
+from user import UserRegister
+from item import Item, ItemList
 
 app = Flask(__name__)
 app.secret_key = 'annie'
@@ -9,27 +11,9 @@ api = Api(app)
 
 jwt = JWT(app, authenticate, identity)
 
-items = []
-
-class Item(Resource):
-    def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return {'item': None}, 404
-
-    def post(self, name):
-            data = request.get_json()
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
-            return item, 201
-
-
-class ItemList(Resource):
-    def get(self):
-        return {'items': items}
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
 
 app.run(port=5000, debug=True)
